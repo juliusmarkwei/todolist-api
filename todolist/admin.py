9d8ca8f73cb1c394b36a8cc990f65060fe59ad2e
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-11-10 22:41:03 +0000

    app models and urls

--- todolist/admin.py
+++ todolist/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.apps import apps
+from .models import UserProfile
 
-# Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'email', 'joined_date']
+    search_fields = ['user__username', 'email']
+    
+    
+models_to_ignore = []
+
+for model in apps.get_models():
+    try:
+        if model._meta.label in models_to_ignore:
+            continue
+        else:
+
+            class modelAdmin(admin.ModelAdmin):
+                list_display = [field.name for field in model._meta.fields]
+
+            admin.site.register(model, modelAdmin)
+    except admin.sites.AlreadyRegistered:
+        pass
--- todolist/models.py
+++ todolist/models.py
@@ -10,29 +10,61 @@ class Category(models.Model):
         return self.name
 
 
-class User(models.Model):
-    username = models.CharField(max_length=50)
-    email = models.EmailField(max_length=50, null=True)
-    password = models.CharField(max_length=20)
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField(default=None, blank=True)
+    joined_date = models.DateField(default=timezone.now)
+
+    def save(self, *args, **kwargs):
+        # Set the email field to the associated user's email when saving
+        self.email = self.user.email
+        super().save(*args, **kwargs)
 
     def __str__(self):
-        return self.username
+        return self.user.username
 
 
-class Task(models.Model):        
-    user = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
+class Task(models.Model):
+    class TaskObject(models.Manager):
+        def get_queryset(self):
+            return super().get_queryset().filter(status="inprogress")
+
+    options = (
+        ("draft", "Draft"),
+        ("inprogress", "In progress"),
+        ("completed", "Completed"),
+    )
+
+    author = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
     category = models.ForeignKey(
         Category, null=False, on_delete=models.CASCADE, default=1
     )
     title = models.CharField(max_length=250)
     description = models.TextField(max_length=500)
-    due_date = models.DateTimeField()
+    due_date = models.DateTimeField(null=True, blank=True)
+    target_date = models.DateTimeField(null=True, blank=True)
+    countdown = models.DurationField(null=True, blank=True)
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(null=False, default=timezone.now)
-    updated_at = models.DateTimeField(auto_now_add=False, auto_now=False, blank=True, null=True)
+    updated_at = models.DateTimeField(
+        auto_now_add=False, auto_now=False, blank=True, null=True
+    )
+    status = models.CharField(max_length=10, choices=options, default="draft")
+
+    def save(self, *args, **kwargs):
+        # Check if due_date is set and is a valid datetime object
+        if self.due_date and isinstance(self.due_date, timezone.datetime):
+            now = timezone.now()
+            time_difference = self.due_date - now
+            self.countdown = time_difference
+        else:
+            print("Date mighthave an error")
+            self.countdown = None
+
+        super().save(*args, **kwargs)
 
     class Meta:
-        ordering = ('-created_at')
-        
+        ordering = ("-created_at",)
+
     def __str__(self):
         return self.title
