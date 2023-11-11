91a52edd32e4aa03999877c402acdd5d0cd2ec30
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-11-11 17:41:11 +0000

    configured url for individual tasks

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import CreateTaskView
+from .views import CreateTaskView, ListTaskView
 
 
 app_name = 'api'
 
 urlpatterns = [
     path('', CreateTaskView.as_view(), name='listcreate'),
-    # path('<int:pk>/', ViewTask.as_view(), name='listview'),
+    path('<int:pk>/', ListTaskView.as_view(), name='listview'),
+    # path('users/', ListUsers.as_view(), name='listusers'),
 ]
