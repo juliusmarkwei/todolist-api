2d63e4f6d3dd49cb5330369aa4b9020b768d9621
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-11-11 17:42:20 +0000

    added read/update/destroy view

--- api/views.py
+++ api/views.py
@@ -1,15 +1,16 @@
 from rest_framework import generics
 from todolist.models import Task, UserProfile
-from .serializers import TaskSerializer, UserProfileSerializer
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from .serializers import TaskSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny
 
 
 class CreateTaskView(generics.ListCreateAPIView):
+    queryset = Task.taskobjects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+class ListTaskView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-    permission_classes = [IsAuthenticated]
-
-
-class ListTaskView(generics.ListAPIView):
-    def get_queryset(self):
-        return super().get_queryset().filter(name="in progress")
+    permission_classes = [IsAuthenticatedOrReadOnly]
