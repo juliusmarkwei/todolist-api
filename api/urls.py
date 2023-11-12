4ea5c9deb2b92f61acec674158bce7b68a246fef
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-11-12 21:59:18 +0000

    trying a boostrap style

--- api/urls.py
+++ api/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import CreateTaskView, ListTaskView
 
 
-app_name = 'api'
+app_name = "api"
 
 urlpatterns = [
-    path('', CreateTaskView.as_view(), name='listcreate'),
-    path('<int:pk>/', ListTaskView.as_view(), name='listview'),
+    path("", CreateTaskView.as_view(), name="listcreate"),
+    path("<int:pk>/", ListTaskView.as_view(), name="listview"),
     # path('users/', ListUsers.as_view(), name='listusers'),
 ]
